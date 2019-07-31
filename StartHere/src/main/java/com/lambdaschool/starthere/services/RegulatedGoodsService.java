7bc86322ba72e5cfcb7398a3411cf5b0b1ed1a31
doc-jones <37349558+doc-jones@users.noreply.github.com>
2019-07-31 17:50:53 -0400

    Breaking: Add 2 Services to consume Sauti marketprice and Sauti regulated goods APIs.

--- StartHere/src/main/java/com/lambdaschool/starthere/services/RegulatedGoodsService.java
+++ StartHere/src/main/java/com/lambdaschool/starthere/services/RegulatedGoodsService.java
@@ -1,11 +1,8 @@
 package com.lambdaschool.starthere.services;
 
-
-
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
-import org.springframework.boot.jackson.JsonObjectSerializer;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
@@ -18,29 +15,29 @@ import org.springframework.web.util.UriComponentsBuilder;
 import java.net.URI;
 
 @Service
-public class MarketPriceService {
+public class RegulatedGoodsService {
 
     private RestTemplate rest = new RestTemplate();
-    public static final String SAUTI_MARKETPRICE_BASE = "http://";
+    public static final String SAUTI_REGULATEDGOODS_BASE = "http://sautiafrica.org/endpoints/api.php?url=v1/regulatedGoods/&type=json";
+    public static final String KEY ="";
 
-    public String getMarketpriceUrl(String) {
+    public String getRegualtedGoodsUrl(String) {
         Gson gson = new Gson();
-        String sauti_marketprice = SAUTI_MARKETPRICE_BASE;
+        String sauti_regulatedgoods = SAUTI_REGULATEDGOODS_BASE + KEY;
         try {
-            URI uri = UriComponentsBuilder.fromUriString(sauti_marketprice).build().encode().toUri();
+            URI uri = UriComponentsBuilder.fromUriString(sauti_regulatedgoods).build().encode().toUri();
             MultiValueMap<String, String> mvm = new LinkedMultiValueMap<>();
             ResponseEntity<String> res = rest.exchange(uri, HttpMethod.GET, new HttpEntity<>(mvm, null), String.class);
 
-            JsonObjectSerializer json = gson.fromJson(res.getBody(), JsonObject.class);
+            JsonObjectSerializer json = gson.fromJson(res.getBody(), JsonObject .class);
 
             JsonArray data = json.getAsJsonArray("data");
             if (data == null || data.size() == 0) {
-                return "https://";
+                return "http://sautiafrica.org/endpoints/api.php?url=v1/regulatedGoods/&type=json";
             }
         } catch (Throwable e) {
             return "https://media.giphy.com/media/ERXGvbDPhLoWs/giphy.gif"
         }
     }
-
 
 }
